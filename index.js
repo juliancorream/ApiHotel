52e46adfe568294535c518c306ca897613ca4217
Juan <juan16david19@gmail.com>
2023-10-09 20:39:37 -0500

    Se crea conexion a la base de datos y se crea la capa modelo y puente a la base de datos

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+import "dotenv/config";
 import { Api } from "./Api.js";
+
+require("dotenv").config();
 
 //Para usar una clase debo crear un objeto
 //Para usar una clasese debe crear una instancia
 //Todo Objeto es una variable
-let servidor = new Api()
+let servidor = new Api();
 
 //Levantar el Servidor
-servidor.levantarServidor()
+servidor.levantarServidor();
---
+++ models/modeloReserva.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const Reserva = new Schema({
+  fechaEntrada: {
+    type: Date,
+    required: true,
+  },
+  fechaSalida: {
+    type: Date,
+    required: true,
+  },
+  cantidadPersonas: {
+    type: Number,
+    required: true,
+  },
+  nombreQuienReserva: {
+    type: String,
+    required: true,
+  },
+  costo: {
+    type: Number,
+    required: false,
+  },
+});
+
+export const modeloReserva = mongoose.model("reservas", Reserva);
